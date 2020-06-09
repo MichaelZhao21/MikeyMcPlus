94ddb8c3d5bf167508c08a112b9f84de1bd9362f
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-09 17:29:18 -0400

    Initial commit

---
+++ src/main/java/xyz/michaelzhao/MikeyMcPlus.java
@@ -0,0 +1,16 @@
+package xyz.michaelzhao;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MikeyMcPlus extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        getLogger().info("Mikey is here!!!!!!!");
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
