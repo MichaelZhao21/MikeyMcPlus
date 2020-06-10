9b198c954a41026da5d25a99e54a6ffb657f50bb
MichaelZhao21 <michaelzhao314@gmail.com>
2020-06-09 20:19:59 -0400

    Added fireworks command

--- src/main/java/xyz/michaelzhao/mikeymcplus/MikeyMcPlus.java
+++ src/main/java/xyz/michaelzhao/mikeymcplus/MikeyMcPlus.java
@@ -3,14 +3,22 @@ package xyz.michaelzhao.mikeymcplus;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MikeyMcPlus extends JavaPlugin {
+
+    public static MikeyMcPlus instance = null;
     @Override
     public void onEnable() {
         super.onEnable();
+        instance = this;
+        this.getCommand("firework").setExecutor(new CommandFirework());
         getLogger().info("Mikey is here!!!!!!!");
     }
 
     @Override
     public void onDisable() {
         super.onDisable();
+    }
+
+    public static MikeyMcPlus getInstance() {
+        return instance;
     }
 }
